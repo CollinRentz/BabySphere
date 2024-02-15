640353199bec05ab17e50fb3281b7fad9bfb4d6a
Collin Rentz <rentzcollin@gmail.com>
2024-02-15 09:43:45 -0700

    Initial Commit

---
+++ BabySphere/BabySphereApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabySphereApp.swift
+//  BabySphere
+//
+//  Created by Collin Rentz on 2/15/24.
+//
+
+import SwiftUI
+
+@main
+struct BabySphereApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BabySphere/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BabySphere
+//
+//  Created by Collin Rentz on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
